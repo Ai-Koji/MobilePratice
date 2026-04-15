fef2fdfb4cf19c709f538dd056f36e2c40b3dcb7
koji <kireev.ivan.8@gmail.com>
2026-04-15 19:16:06 +0700

    pratice13: base layout

--- pratice13/app/(tabs)/index.tsx
+++ pratice13/app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
 import { StyleSheet } from 'react-native';
-import { View, Text, Image } from 'react-native';
+import { View, Image, ScrollView } from 'react-native';
+// import { ScrollView } from 'react-native-reanimated/lib/typescript/Animated';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
 
         {/* navbar */}
-        <View style={styles.navbar}>
+        <View style={styles.columns}>
             <View style={styles.column}>
                 <View style={{...styles.navItem, ...styles.imageContainer}}>
                     <Image 
@@ -23,7 +24,7 @@ export default function HomeScreen() {
                 <View style={{...styles.navItem, ...styles.imageContainer}}>
                     <Image 
                     style={styles.image}
-                    source={require('../../assets/images/phone.svg')}/>
+                    source={require('../../assets/images/phone.png')}/>
                 </View>
                 <View style={{...styles.navItem, ...styles.imageContainer}}>
                     <Image 
@@ -33,10 +34,93 @@ export default function HomeScreen() {
             </View>
         </View>
         
-
+        <ScrollView
+          showsVerticalScrollIndicator={false} // Скрывает полосу прокрутки (по желанию)
+          contentContainerStyle={styles.scrollContent} // Стили для внутреннего контейнера
+        >
         {/* gallery */}
-
-
+          <View style={styles.content}>
+              <View style={styles.contentColumn}>
+                  <View style={styles.imageContainer}>
+                      <Image 
+                      style={styles.imageGallery}
+                      source={require('../../assets/images/1.png')}/>
+                  </View>
+                  <View style={styles.imageContainer}>
+                      <Image 
+                      style={styles.imageGallery}
+                      source={require('../../assets/images/1.png')}/>
+                  </View>
+                  <View style={styles.imageContainer}>
+                      <Image 
+                      style={styles.imageGallery}
+                      source={require('../../assets/images/1.png')}/>
+                  </View>
+                  <View style={styles.imageContainer}>
+                      <Image 
+                      style={styles.imageGallery}
+                      source={require('../../assets/images/1.png')}/>
+                  </View>
+                  <View style={styles.imageContainer}>
+                      <Image 
+                      style={styles.imageGallery}
+                      source={require('../../assets/images/1.png')}/>
+                  </View>
+                  <View style={styles.imageContainer}>
+                      <Image 
+                      style={styles.imageGallery}
+                      source={require('../../assets/images/1.png')}/>
+                  </View>
+              </View>
+              <View style={styles.contentColumn}>
+                  <View style={styles.imageContainer}>
+                      <Image 
+                      style={styles.imageGallery}
+                      source={require('../../assets/images/1.png')}/>
+                  </View>
+                  <View style={styles.imageContainer}>
+                      <Image 
+                      style={styles.imageGallery}
+                      source={require('../../assets/images/1.png')}/>
+                  </View>
+                  <View style={styles.imageContainer}>
+                      <Image 
+                      style={styles.imageGallery}
+                      source={require('../../assets/images/1.png')}/>
+                  </View>
+                  <View style={styles.imageContainer}>
+                      <Image 
+                      style={styles.imageGallery}
+                      source={require('../../assets/images/1.png')}/>
+                  </View>
+                  <View style={styles.imageContainer}>
+                      <Image 
+                      style={styles.imageGallery}
+                      source={require('../../assets/images/1.png')}/>
+                  </View>
+                  <View style={styles.imageContainer}>
+                      <Image 
+                      style={styles.imageGallery}
+                      source={require('../../assets/images/1.png')}/>
+                  </View>
+                  <View style={styles.imageContainer}>
+                      <Image 
+                      style={styles.imageGallery}
+                      source={require('../../assets/images/1.png')}/>
+                  </View>
+                  <View style={styles.imageContainer}>
+                      <Image 
+                      style={styles.imageGallery}
+                      source={require('../../assets/images/1.png')}/>
+                  </View>
+                  <View style={styles.imageContainer}>
+                      <Image 
+                      style={styles.imageGallery}
+                      source={require('../../assets/images/1.png')}/>
+                  </View>
+              </View>
+          </View>
+        </ScrollView>
     </View>
   );
 }
@@ -54,25 +138,48 @@ const styles = StyleSheet.create({
     fontSize: 20
   },
   image: {
-    width: 60,
-    height: 60
+    width: 50,
+    height: 50
+  },
+  imageGallery: {
+    width: 150,
+    height: 150  
   },
   imageContainer: {
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    borderRadius: 10
   },
   column: {
     width: 150,
     justifyContent: 'space-between',
     flexDirection: 'row'
   },
-  navbar: {
+  columnImg: {
+    width: 300
+  },
+  columns: {
     flexDirection: 'row',
-    gap: 40
+    gap: 40,
+    marginBottom: 10
   },
   navItem: {
     width: 70,
     height: 70,
     backgroundColor: '#d9d9d9'
-  }
+  },
+  content: {
+    flexDirection: 'row',
+    gap: 40,
+    marginTop: 40
+  },
+  contentColumn: {
+    width: 150,
+    gap: 20
+  },
+  scrollContent: {
+    alignItems: 'center', // Центрируем элементы внутри скролла
+    paddingTop: 50,       // Тот самый отступ вместо marginTop
+    paddingBottom: 40,    // Отступ снизу, чтобы последний элемент не прилипал
+  },
 });
