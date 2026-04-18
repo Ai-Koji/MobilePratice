44f15d3253eb38e3b30124ef4a082243fe1c310f
koji <kireev.ivan.8@gmail.com>
2026-04-18 14:41:57 +0700

    feat: refresher

--- pratice15/app/(tabs)/index.tsx
+++ pratice15/app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
-import { StyleSheet, ScrollView, View, Text } from 'react-native';
+import { StyleSheet, ScrollView, RefreshControl, View, Text } from 'react-native';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 
 import * as Location from 'expo-location';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 
 
 interface Location {
@@ -50,7 +50,16 @@ async function getWeather(city : string, lang : string) {
   let url = `https://api.weatherapi.com/v1/current.json?key=${process.env.EXPO_PUBLIC_API_KEY}&q=${city}&lang=${lang}&aqi=no`
   console.log('url: ', url);
 
-  const res = (await fetch(url)).json();
+  let res;
+  try {
+    res = (await fetch(url));
+    console.log(res)
+    res = res.json()
+  }
+  catch(error) {
+    console.log(error);
+    res = {};
+  }
 
   return res;
 }
@@ -59,6 +68,8 @@ async function getWeather(city : string, lang : string) {
 export default function HomeScreen() {
   const [city, setCity] = useState('Москва');
   const [weather, setWeather] = useState({});
+  const [refreshing, setRefreshing] = useState(false);
+
   useEffect(() => {
     const getCity = async () => {
       let location = await getLoc();
@@ -67,6 +78,13 @@ export default function HomeScreen() {
     }
     getCity();
   }, [])
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 800);
+  }, []);
 
   useEffect(() => {
     const weather = async () => {
@@ -78,7 +96,11 @@ export default function HomeScreen() {
 
   console.log('weather: ', weather);
   return (
-    <ScrollView>
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+    >
       <View style={styles.mainStatus}>
         <ThemedText style={{...styles.mainInfo, ...styles.mainInfoHeader}}>
           <Text>{weather.temp_c}</Text> ℃
